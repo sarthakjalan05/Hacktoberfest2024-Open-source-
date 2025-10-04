c69e7a6d3dd6eed46936420051b3779f24f41b37
Sarthak Jalan <sarthakjalan06@gmail.com>
2025-10-04 21:27:07 +0530

    added java code

---
+++ BankAccount.java
@@ -0,0 +1,31 @@
+public class BankAccount {
+    private int accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(int accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+}
